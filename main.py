2730776f7802ad1391b83e25f6397aa5d8472e66
HatGuy68 <irahul.2k@gmail.com>
2023-01-25 20:28:32 +0530

    Basic Styling added to main app

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.absolute() / "static"),
+    name="static",
+)
+
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
